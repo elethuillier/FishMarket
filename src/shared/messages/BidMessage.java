dd8350d868b287a740b0d9d00531241cfad67ba3
Araken64 <elian.araken@gmail.com>
2021-02-23 21:03:06 +0100

    BidMessage structure correction

--- src/shared/messages/BidMessage.java
+++ src/shared/messages/BidMessage.java
@@ -1,19 +1,27 @@
 package shared.messages;
 
+import jade.core.AID;
+
 public class BidMessage {
     private final int auction_id;
-    private final double offer;
+    private final String description;
+    private final double price;
+    private final AID buyer_id;
 
-    public BidMessage(int auction_id, double offer) {
+    public BidMessage(int auction_id, String description, double price, AID buyer_id) {
         this.auction_id = auction_id;
-        this.offer = offer;
+        this.description = description;
+        this.price = price;
+        this.buyer_id = buyer_id;
     }
 
     public int getAuctionId() {
         return auction_id;
     }
 
-    public double getOffer() {
-        return offer;
-    }
+    public String getDescription() { return description; }
+
+    public double getPrice() { return price; }
+
+    public AID getBuyerId() { return buyer_id; }
 }
