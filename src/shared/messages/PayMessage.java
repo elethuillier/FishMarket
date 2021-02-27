0ffa6514e9ecae6a8142ebd7ac2f1a668dc7c8de
Araken64 <elian.araken@gmail.com>
2021-02-27 19:17:12 +0100

    fix(PayMessage): add price field

--- src/shared/messages/PayMessage.java
+++ src/shared/messages/PayMessage.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 
 public class PayMessage implements Serializable {
     private final int auction_id;
+    private final double price;
 
-    public PayMessage(int auction_id) {
+    public PayMessage(int auction_id, double price) {
         this.auction_id = auction_id;
+        this.price = price;
     }
 
     public int getAuctionId() {
         return auction_id;
     }
+
+    public double getPrice() { return price; }
 }
