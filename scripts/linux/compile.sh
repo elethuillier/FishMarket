abdd64af8d8a8f387c3dbbe9a4ba5a3ca04cadbb
Araken64 <elian.araken@gmail.com>
2021-02-28 23:11:04 +0100

    fix(compile.sh): wrong path

--- scripts/linux/compile.sh
+++ scripts/linux/compile.sh
@@ -1,5 +1,5 @@
 cd ../../
-mkdir -p out
-javac -encoding UTF-8 -cp lib/jade.jar -d out @files.txt
+mkdir -p out/production/FishMarket
+javac -encoding UTF-8 -cp lib/jade.jar -d out/production/FishMarket @files.txt
 mkdir -p out/production/FishMarket/view
 cp -r src/view out/production/FishMarket/view
