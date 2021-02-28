346b5c736350717b138359098566fed924aafe3c
Araken64 <elian.araken@gmail.com>
2021-02-28 22:55:36 +0100

    fix(compile.sh): recursive copy

--- scripts/linux/compile.sh
+++ scripts/linux/compile.sh
@@ -2,4 +2,4 @@ cd ../../
 mkdir -p out
 javac -encoding UTF-8 -cp lib/jade.jar -d out @files.txt
 mkdir -p out/production/FishMarket/view
-cp src/view out/production/FishMarket/view
+cp -r src/view out/production/FishMarket/view
