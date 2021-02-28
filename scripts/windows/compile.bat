3bc2e11a09af8d60947e2a5626326d8951eb886f
Araken64 <elian.araken@gmail.com>
2021-02-28 21:19:15 +0100

    fix(scripts): compilation and parallelism

--- scripts/windows/compile.bat
+++ scripts/windows/compile.bat
@@ -1,4 +1,4 @@
 cd ../../
-if not exist out (mkdir out)
-javac -encoding UTF-8 -cp lib/jade.jar -d out @files.txt
-
+if not exist out (mkdir out\production\FishMarket)
+javac -encoding UTF-8 -cp lib/jade.jar -d out\production\FishMarket @files.txt
+xcopy /e /i /y src\view out\production\FishMarket\view
