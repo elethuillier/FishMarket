7a8f7b755199519850a1729e75394e90aaac7887
Araken64 <elian.araken@gmail.com>
2021-02-28 23:33:01 +0100

    fix(compile.sh): useless directory creation

--- scripts/linux/compile.sh
+++ scripts/linux/compile.sh
@@ -1,5 +1,4 @@
 cd ../../
 mkdir -p out/production/FishMarket
 javac -encoding UTF-8 -cp lib/jade.jar -d out/production/FishMarket @files.txt
-mkdir -p out/production/FishMarket/view
-cp -r src/view out/production/FishMarket/view
+cp -r src/view out/production/FishMarket
